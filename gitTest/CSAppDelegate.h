f80449d42554a9782592e71f4e805ba2ebe3a1a2
Jonathan Trubshaw <JTrubshaw@Jonnys-MacBook-Pro.local>
2015-01-01 18:15:01 -0600

    Initial Commit

---
+++ gitTest/CSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSAppDelegate.h
+//  gitTest
+//
+//  Created by Jonathan Trubshaw on 1/1/15.
+//  Copyright (c) 2015 Capstone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
