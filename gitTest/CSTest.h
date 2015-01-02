edf146fce37996c0b28c4fa44d7cd2444598fe15
jontrubshaw <jontrubshaw@gmail.com>
2015-01-01 19:58:56 -0600

    added a test class

---
+++ gitTest/CSTest.h
@@ -0,0 +1,13 @@
+//
+//  CSTest.h
+//  gitTest
+//
+//  Created by Jonathan Trubshaw on 1/1/15.
+//  Copyright (c) 2015 Capstone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSTest : NSObject
+
+@end
---
+++ gitTest/CSTest.m
@@ -0,0 +1,13 @@
+//
+//  CSTest.m
+//  gitTest
+//
+//  Created by Jonathan Trubshaw on 1/1/15.
+//  Copyright (c) 2015 Capstone. All rights reserved.
+//
+
+#import "CSTest.h"
+
+@implementation CSTest
+
+@end
